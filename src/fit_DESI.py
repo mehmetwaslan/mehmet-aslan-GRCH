4708c5ea5219fd103710ee90d34f14e57ecccd51
mehmetwaslan <mehmetwaslan@gmail.com>
2025-11-12 21:32:25 +0000

    Create fit_DESI.py

---
+++ src/fit_DESI.py
@@ -0,0 +1,9 @@
+# fit_DESI.py
+# GRCH vs DESI Year 1 fσ8 comparison
+# Author: Mehmet Aslan, 2025
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
+
+print("GRCH–DESI fit module loaded.")
