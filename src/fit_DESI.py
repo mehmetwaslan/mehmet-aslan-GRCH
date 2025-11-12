240f9f5f57cf5ae4de5b21e92987a1893d46cc1d
mehmetwaslan <mehmetwaslan@gmail.com>
2025-11-12 21:44:52 +0000

    Delete src/fit_DESI.py

--- src/fit_DESI.py
+++
@@ -1,9 +0,0 @@
-# fit_DESI.py
-# GRCH vs DESI Year 1 fσ8 comparison
-# Author: Mehmet Aslan, 2025
-
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.optimize import minimize
-
-print("GRCH–DESI fit module loaded.")
